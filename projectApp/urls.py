b1a91c638b8556631acf5ba993b5a1eae9f03dfb
Ramish Altaf <rhmi1243@gmail.com>
2021-12-29 19:20:53 +0500

    REST Framework

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -2,12 +2,16 @@ from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = DefaultRouter()
 router.register(r'Item', views.ItemListViews)
 router.register(r'User', views.UserListViews)
 
+#router.include_format_suffixes = False
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+#urlpatterns = format_suffix_patterns(urlpatterns,allowed=['html'])
